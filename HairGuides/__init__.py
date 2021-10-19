5d6b4eb1e3e681a1961a4b27ca72e443973ae9bf
Alan O'Cull <masteralan2001@gmail.com>
2021-10-19 15:17:23 -0400

    Skeleton file structure

---
+++ HairGuides/__init__.py
@@ -0,0 +1,21 @@
+bl_info = {
+    "name" : "Blender Child Guides",
+    "author" : "Alan O'Cull, cellomonster",
+    "description" : "desc here",
+    "blender" : (2, 93, 2),
+    "version" : (0, 0, 1),
+    "location" : "",
+    "warning" : "",
+    "category" : "Particles"
+}
+
+import bpy
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # blender-child-guides
 Create guide hair particles from children hair particles (for better control over clumping, etc)
+
+## Installation
+This is a Blender plugin. Simply zip up the "HairGuides" folder and then, in Blender, go to `Edit > Preferences > Add-ons` and press Install Add-on. Install the zip folder, and the plugin should be present.
